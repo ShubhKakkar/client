a5c5adac187be14f2db4825d7b7e07c7c2eb94e6
ShubhKakkar <86162630+ShubhKakkar@users.noreply.github.com>
2023-12-30 22:26:27 +0530

    favicon added

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,6 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+      </head>
       <body className={inter.className}>
         <SocketProvider>{children}</SocketProvider>
       </body>
